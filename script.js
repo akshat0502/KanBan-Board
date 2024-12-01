08151e5e1f2100d2c0a8ce81278937c42b2fd735
Akshat Saxena <akshat.seth0508@gmail.com>
2024-12-01 21:29:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    const data = event.dataTransfer.getData("text");
+    const card = document.getElementById(data);
+    event.target.appendChild(card);
+}
